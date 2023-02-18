ce572d3b9588766e21a439bfa532156f4f44d8a3
MSK1206 <phantom7216@yahoo.co.jp>
2023-02-18 17:22:45 +0900

    Add Contact Page : Formrun .

---
+++ src/pages/contact.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import styles from "../styles/Contact.module.css";
+
+const Contact: React.FC = () => {
+  const FormrunID: string = `${process.env.FORMRUN_FORM_ID}`;
+  useEffect(() => {
+    const head = document.getElementsByTagName("head")[0] as HTMLElement;
+    const scriptUrl = document.createElement("script");
+    scriptUrl.src = "https://sdk.form.run/js/v2/embed.js";
+    head.appendChild(scriptUrl);
+    return () => {
+      head.removeChild(scriptUrl);
+    };
+  }, []);
+  return (
+    <main className={styles.main}>
+      <div className={styles.formrun_width}>
+        <div
+          className="formrun-embed"
+          data-formrun-form={FormrunID}
+          data-formrun-redirect="true"
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Contact;
