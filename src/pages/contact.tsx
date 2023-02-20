8151340e1dc9ec28a670dbb2e95adf4b58eb6b12
MSK1206 <phantom7216@yahoo.co.jp>
2023-02-21 00:51:35 +0900

    contact page fix .

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,27 +1,42 @@
+import { SeoHead } from "@/components/seohead";
 import { useEffect } from "react";
 import styles from "../styles/Contact.module.css";
 
-const Contact: React.FC = () => {
-  const FormrunID: string = `${process.env.FORMRUN_FORM_ID}`;
+type Props = {
+  FormrunID: string;
+};
+
+const Contact: React.FC<Props> = () => {
+  const FormrunID = `${process.env.NEXT_PUBLIC_FORMRUN_FORM_ID}`;
   useEffect(() => {
     const head = document.getElementsByTagName("head")[0] as HTMLElement;
     const scriptUrl = document.createElement("script");
     scriptUrl.src = "https://sdk.form.run/js/v2/embed.js";
     head.appendChild(scriptUrl);
+    console.log("scriptよみこんだよ");
     return () => {
       head.removeChild(scriptUrl);
     };
   }, []);
   return (
-    <main className={styles.main}>
-      <div className={styles.formrunwidth}>
-        <div
-          className="formrun-embed"
-          data-formrun-form={FormrunID}
-          data-formrun-redirect="true"
-        />
-      </div>
-    </main>
+    <>
+      <SeoHead
+        title="contact"
+        titleTemplate="My-WebSite"
+        description="Next.js + TypeScript Sample Project."
+        ogType="website"
+        imgUrl="/ogimg_sample.jpg"
+      />
+      <main className={styles.main}>
+        <div className={styles.formrunwidth}>
+          <div
+            className="formrun-embed"
+            data-formrun-form={FormrunID}
+            data-formrun-redirect="true"
+          />
+        </div>
+      </main>
+    </>
   );
 };
 
